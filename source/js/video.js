d83a1294ac8c18fd655b84c77a091d94f265688d
Antonina Sorokan <atretyakova28@yandex.ru>
2024-07-20 19:22:27 +0300

    Добавляет изменения

---
+++ source/js/video.js
@@ -0,0 +1,5 @@
+const playButton = document.querySelector('.about__button');
+const videoBackground = document.querySelector('.about__video-background');
+const videoContainer = document.querySelector('.about__video');
+
+playButton.addEventListener('click', function());
